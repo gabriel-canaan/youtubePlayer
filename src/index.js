c49053dbe6f4e9731d2f5b64e96c35c508765033
gabriel <777canaan@gmail.com>
2017-07-25 23:27:19 +1200

    event handlers

--- src/index.js
+++ src/index.js
@@ -3,28 +3,33 @@ import ReactDOM from 'react-dom'
 import YTsearch from 'youtube-api-search'
 import SearchBar from './components/search_bar'
 import VideoList from './components/video_list'
+import VideoDetail from './components/video_detail'
 const API_KEY = 'AIzaSyBcEVXUAWMKfxSr9NisVAFGTU-m8I8vf-M'
 
 class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      videos: []
+      videos: [],
+      selectedVideo: null
     }
 
-    YTsearch({
-      key: API_KEY,
-      term: 'surfboards'
-    }, (videos) => {
-      this.setState({videos})
+    YTsearch({key: API_KEY,term: 'surfboards'}, (videos) => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      })
     })
   }
 
   render() {
     return (
       <div>
-        <SearchBar/>
-        <VideoList videos={this.state.videos} />
+        <SearchBar />
+        <VideoDetail video={this.state.selectedVideo}/>
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+          videos={this.state.videos} />
       </div>
     )
   }
